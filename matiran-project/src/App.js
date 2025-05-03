3d69ef81f5a0919b004cbed648923ac050091f09
mahdieh <mahdiehakbari16@gmail.com>
2025-05-03 21:30:36 +0330

    create react app

---
+++ matiran-project/src/App.js
@@ -0,0 +1,15 @@
+import { Button } from '@mui/material';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Hello MUI</h1>
+      <Button variant='contained' color='primary'>
+        Click me
+      </Button>
+    </div>
+  );
+}
+
+export default App;
